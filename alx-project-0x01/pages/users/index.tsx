a0dd7ee8ef3cf92cec096b8991aff06160fdcb19
Opeyemi <eco.ajibade@gmail.com>
2025-09-20 16:07:54 +0100

    alx-project-0x01

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "@/components/common/Button";
+import Header from "@/components/layout/Header";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl mb-4">Users</h1>
-      <Button />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-6">
+        <h1 className="text-3xl mb-4">Users</h1>
+        <Button />
+      </main>
     </div>
   );
 };
