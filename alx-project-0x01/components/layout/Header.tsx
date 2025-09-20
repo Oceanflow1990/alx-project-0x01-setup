c10ac8ba0a0ab428eaa530e15344b0f2ddbd18ee
Opeyemi <eco.ajibade@gmail.com>
2025-09-20 15:56:56 +0100

    alx-project-0x01

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="p-4 bg-gray-200 text-center">
+      <h1 className="text-2xl font-bold">Header</h1>
+    </header>
+  );
+};
+
+export default Header;
